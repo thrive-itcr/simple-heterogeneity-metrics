0b2760dcc523f172b878a1c548f765f10684dbe0
Sarachan, Brion D <sarachan@ge.com>
2018-01-17 13:04:11 -0500

    Changed name of Docker image to have thrive prefix.  Added no_proxy to build line in README.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 To build the Docker image:
 ```sh
-$ docker build -t rt106/heterogeneity-metrics --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg no_proxy=$no_proxy .
+$ docker build -t thrive/heterogeneity-metrics --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg no_proxy=$no_proxy .
 ```
--- algrebuild.sh
+++ algrebuild.sh
@@ -3,11 +3,10 @@
 
 echo ""
 echo "Removing algorithm image."
-docker rmi rt106/heterogeneity-metrics
+docker rmi thrive/heterogeneity-metrics
 
 echo ""
 echo "Building algorithm image."
-docker build -t rt106/heterogeneity-metrics --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy .
-
+docker build -t thrive/heterogeneity-metrics --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg no_proxy=$no_proxy .
 echo ""
 docker images
